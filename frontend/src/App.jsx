39d4b6afe0c23d901db8c976b9b493994b5e15ed
Izzatbek <abdusharipovizzat03@gmail.com>
2025-06-30 12:12:11 +0500

    netflix clone

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Landing from './pages/Landing'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <ToastContainer/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/landing' element={<Landing/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/forgot-password' element={<ForgotPassword/>} />
+      <Route path='/reset-password' element={<ResetPassword/>} />
+    </Routes>
+    </>
   )
 }
 
